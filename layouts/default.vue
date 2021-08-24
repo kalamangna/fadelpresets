1be209deccbda1127ffbd6a0665f6c022c16529b
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2021-08-24 23:41:16 +0800

    Added tailwind ccss

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,22 @@
 <template>
   <div>
-    <Nuxt />
+    <!-- HEADER -->
+    <nav class="p-4 bg-gray-800 text-white">
+      <div class="font-bold">
+        FADELPRESETS
+      </div>
+    </nav>
+
+    <div class="container mx-auto p-8">
+      <Nuxt />
+    </div>
+
+    <!-- FOOTER -->
+    <footer class="p-2 bg-gray-800 text-white text-sm w-full fixed bottom-0">
+      &copy; {{ new Date().getFullYear() }} -
+      <a href="https://github.com/kalamangna" target="_blank"
+        >github.com/kalamangna</a
+      >
+    </footer>
   </div>
 </template>
-
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
